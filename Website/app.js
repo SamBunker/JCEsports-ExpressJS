4becb766d9d093e57d943b23b2bcbc3b6fa68af2
Not Sam <samuelbunker@live.com>
2023-12-07 13:07:02 -0500

    Created Registration System

--- Website/app.js
+++ Website/app.js
@@ -1,9 +1,9 @@
 const express = require('express');
-// const connection = require('./js/db');
-// const adminBro = require('admin-bro');
 var app = express();
 var handlebars = require('express-handlebars').create({defaultLayout: 'main'});
-// const mysql = require('mysql');
+const mysql = require('mysql');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
 const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
@@ -12,19 +12,26 @@ app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
 app.set('port', process.env.PORT || 3000);
 
-// const AdminBroOptions = {
-//     databases: [connection],
-//     resources: [], // Define your resource objects here
-//   };
-// const admin = new adminBro(AdminBroOptions);
 
-// connection.connect((err) => {
-//   if (err) {
-//     console.error('Error connecting to MySQL:', err);
-//     return;
-//   }
-//   console.log('Connected to MySQL database');
-// });
+// Connect to MySQL database
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'esports'
+  });
+  
+  connection.connect((err) => {
+    if (err) {
+      console.error('Error connecting to MySQL:', err);
+      return;
+    }
+    console.log('Connected to MySQL database');
+  });
+  
+app.get('/admin', (req, res) => {
+    res.render('admin');
+});
 
 app.get('/teamlist', (req, res) => {
     res.render('teamlist');
@@ -34,25 +41,57 @@ app.get('/about', (req, res) => {
     res.render('about');
 });
 
-// Define route to handle form submission
-app.post('/teamlist', (req, res) => {
-    const email = req.body.email;
-
-    // Insert the email into the database
-    const sql = 'INSERT INTO subscribers (email) VALUES (?)';
-    connection.query(sql, [email], (err, result) => {
-        if (err) {
-        console.error('Error inserting email:', err);
-        res.status(500).redirect('/thank-you');
-        return;
-        }
-
-        res.status(200).redirect('/thank-you');
-    });
+app.get('/register', (req, res) => {
+    res.render('register');
 });
 
-app.get('/thank-you', (req, res) => {
-    res.render('thank-you');
+app.post('/register', (req, res) => {
+    const emailExistsQuery = 'SELECT * FROM users WHERE email = ?';
+    const sanitizedEmail = sanitizeInput(req.body.email);
+    connection.query(emailExistsQuery, [sanitizedEmail], (err, results) => {
+        if (err) {
+            res.send("Error parsing database.");
+        } else if (results.length > 0) {
+            res.send('Email already in use.');
+            return;
+        } else {
+            bcrypt.genSalt(saltRounds, function(err, salt) {
+                bcrypt.hash(sanitizeInput(req.body.password), salt, function(err, hash) {
+                if (err) {
+                    console.error('Error hashing password:', err);
+                    res.status(500).send('Error hashing password!');
+                    return;
+                }
+                const hashPassword = hash;
+
+                const sql = 'INSERT INTO users (username, email, password, authority) VALUES (?, ?, ?, ?)';
+                connection.query(sql, [sanitizeInput(req.body.username), sanitizeInput(req.body.email), hashPassword, req.body.authority], (err, result) => {
+                if (err) {
+                    console.error('Error inserting user:', err);
+                    res.status(500).send('Error!');
+                    return;
+                }
+                res.status(200).send("Registration Successful");
+                });
+            });
+        });
+    }
+});});
+
+// Define route to handle form submission
+app.post('/admin', (req, res) => {
+    // Insert the email into the database
+    const sql = 'INSERT INTO players (number, name, gamertag, team, position, grade, hometown_highschool, country_code) VALUES (?, ?, ?, ?, ?, ?, ?, ?)';
+    connection.query(sql, [sanitizeInput(req.body.playernumber), sanitizeInput(req.body.playername), sanitizeInput(req.body.playergamertag), 
+        sanitizeInput(req.body.playerteam), sanitizeInput(req.body.playerposition), sanitizeInput(req.body.playergrade), 
+        sanitizeInput(req.body.playerhometown), sanitizeInput(req.body.country)], (err, result) => {
+        if (err) {
+            console.error('Error inserting email:', err);
+            res.status(500).send('Error!');
+            return;
+        }
+        res.status(200).send('Thank you for submitting');
+    });
 });
 
 // Define route to render the form page
@@ -77,3 +116,7 @@ app.listen(app.get('port'), function() {
     console.log('Started express app on http://localhost:' +
         app.get('port') + '; press Ctrl-C to terminate.');
 });
+
+function sanitizeInput(input) {
+    return input.replace(/[^a-zA-Z0-9\-\_\.\@]/g, '');
+  }
